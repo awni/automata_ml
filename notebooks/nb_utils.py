e65dd9b9f05ba9ec12dc4eda65f28595bc94b971
Awni Hannun <awni.hannun@gmail.com>
2021-07-22 09:02:37 -0700

    notebooks for tutorial

---
+++ notebooks/nb_utils.py
@@ -0,0 +1,19 @@
+from IPython.core.display import display, HTML
+
+def init():
+    return display(
+    HTML("""
+        <style>
+             .container { width:900px !important; }
+             .figure {display:table; margin:auto;}
+             .img {float: left; margin: 10px;}
+             .caption {display:table-caption;caption-side:bottom;text-align:justify;color: rgba(0,0,0,0.6);}
+             td { background: white;}
+            .table_caption {
+                font-size:14px;
+                float: left;
+                text-align: justify;
+                color: rgba(0,0,0,0.6);
+            }
+        </style>
+        """))
